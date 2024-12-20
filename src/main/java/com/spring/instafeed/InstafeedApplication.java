7c82058320700ccca68b319d587d245774360499
backswan0 <rnrwprh3434@gmail.com>
2024-12-20 20:21:05 +0900

    test commit

---
+++ src/main/java/com/spring/instafeed/InstafeedApplication.java
@@ -0,0 +1,13 @@
+package com.spring.instafeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstafeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstafeedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/instafeed/InstafeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.instafeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstafeedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
