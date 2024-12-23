5d2295c21b940cea8afeaa03b3f784a79c461f06
backswan0 <rnrwprh3434@gmail.com>
2024-12-23 15:59:57 +0900

    refactor: 팔로잉의 연관관계를 유저에서 프로필로 변경

--- src/main/java/com/spring/instafeed/follower/entity/Follower.java
+++ src/main/java/com/spring/instafeed/follower/entity/Follower.java
@@ -2,6 +2,7 @@ package com.spring.instafeed.follower.entity;
 
 import com.spring.instafeed.base.BaseFollowerEntity;
 import com.spring.instafeed.base.Status;
+import com.spring.instafeed.profile.entity.Profile;
 import com.spring.instafeed.user.entity.User;
 import jakarta.persistence.*;
 import lombok.Getter;
@@ -15,12 +16,12 @@ public class Follower extends BaseFollowerEntity {
     private Long id;
 
     @ManyToOne
-    @JoinColumn(name = "sender_id")
-    private User sender;
+    @JoinColumn(name = "sender_profile_id")
+    private Profile sender;
 
     @ManyToOne
-    @JoinColumn(name = "receiver_id")
-    private User receiver;
+    @JoinColumn(name = "receiver_profile_id")
+    private Profile receiver;
 
     @Column(columnDefinition = "VARCHAR(32)")
     @Enumerated(EnumType.STRING)
@@ -28,7 +29,7 @@ public class Follower extends BaseFollowerEntity {
 
     public Follower() {}
 
-    public Follower(User sender, User receiver, Status status) {
+    public Follower(Profile sender, Profile receiver, Status status) {
         this.sender = sender;
         this.receiver = receiver;
         this.status = status;
--- src/main/java/com/spring/instafeed/follower/service/FollowerServiceImpl.java
+++ src/main/java/com/spring/instafeed/follower/service/FollowerServiceImpl.java
@@ -4,28 +4,28 @@ import com.spring.instafeed.base.Status;
 import com.spring.instafeed.follower.dto.response.FollowerResponseDto;
 import com.spring.instafeed.follower.entity.Follower;
 import com.spring.instafeed.follower.repository.FollowerRepository;
-import com.spring.instafeed.user.entity.User;
+import com.spring.instafeed.profile.entity.Profile;
+import com.spring.instafeed.profile.repository.ProfileRepository;
 import com.spring.instafeed.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class FollowerServiceImpl implements FollowerService {
     private final FollowerRepository followerRepository;
-    private final UserRepository userRepository;
+    private final ProfileRepository profileRepository;
 
     @Override
     public FollowerResponseDto sendFollowRequest(Long senderId, Long receiverId) {
 
-        User sendingUser = userRepository.findByIdOrElseThrow(senderId);
-        User receivingUser = userRepository.findByIdOrElseThrow(receiverId);
+        Profile sendingProfile = profileRepository.findByIdOrElseThrow(senderId);
+        Profile receivingProfile = profileRepository.findByIdOrElseThrow(receiverId);
 
-        Follower follower = new Follower(sendingUser, receivingUser, Status.PENDING);
+        Follower follower = new Follower(sendingProfile, receivingProfile, Status.PENDING);
 
         Follower savedFollower = followerRepository.save(follower);
 
