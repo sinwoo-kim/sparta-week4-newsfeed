b27a65925984feffcdc4763450f1692e70d94cc5
Groo <woohyeon3382@naver.com>
2024-12-24 09:53:09 +0900

    feat: 컨트롤러 login 메서드 구현

--- src/main/java/com/spring/instafeed/auth/controller/LoginController.java
+++ src/main/java/com/spring/instafeed/auth/controller/LoginController.java
@@ -1,6 +1,14 @@
 package com.spring.instafeed.auth.controller;
 
+import com.spring.instafeed.auth.dto.request.LoginRequestDto;
+import com.spring.instafeed.auth.dto.response.AccessTokenResponseDto;
+import com.spring.instafeed.auth.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +16,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/users")
 public class LoginController {
+
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<AccessTokenResponseDto> login(@RequestBody LoginRequestDto dto) {
+        AccessTokenResponseDto accessTokenResponseDto = authService.loginUser(dto);
+        String token = accessTokenResponseDto.accessToken();
+
+        ResponseCookie cookie = ResponseCookie.from("token", token)
+                .httpOnly(true)
+                .secure(true)
+                .maxAge(60 * 60)
+                .build();
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .body(accessTokenResponseDto);
+    }
 }
