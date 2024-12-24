7de3d7859c82021545290e9f941ea09322525495
Groo <woohyeon3382@naver.com>
2024-12-24 09:52:11 +0900

    fix: .equals 메서드에서 BCryptUtil.matches() 메서드로 변경
    - 기존 정의 해놓은 BCryptUtil 을 사용하지 않아서 암호화된 비밀번호와 원본 비밀번호가 같아도 false 가 나오는 오류 수정

--- src/main/java/com/spring/instafeed/auth/domain/Password.java
+++ src/main/java/com/spring/instafeed/auth/domain/Password.java
@@ -16,7 +16,7 @@ public class Password {
     }
 
     /**
-     * 원본 비밀번호를 받아서 암호화된 Password 객체 생성
+     * 입력받은 비밀번호를 암호화 후 Password 객체 생성
      */
     public static Password generateEncryptedPassword(String rawPassword) {
         return new Password(BCryptUtil.encrypt(rawPassword));
@@ -33,6 +33,6 @@ public class Password {
      * 입력된 비밀번호가 저장된 비밀번호와 일치하는지 검증
      */
     public boolean matchPassword(String rawPassword) {
-        return encryptedPassword.equals(BCryptUtil.encrypt(rawPassword));
+        return BCryptUtil.matches(rawPassword, encryptedPassword);
     }
 }
