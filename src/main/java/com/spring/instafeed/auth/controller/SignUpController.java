ae90827dd0184c3925eb14e426ed8d3004ad3674
Groo <woohyeon3382@naver.com>
2024-12-24 09:52:53 +0900

    feat: 컨트롤러 signUp 메서드 구현

--- src/main/java/com/spring/instafeed/auth/controller/SignUpController.java
+++ src/main/java/com/spring/instafeed/auth/controller/SignUpController.java
@@ -1,11 +1,49 @@
 package com.spring.instafeed.auth.controller;
 
+import com.spring.instafeed.auth.dto.request.SignUpRequestDto;
+import com.spring.instafeed.auth.dto.response.AccessTokenResponseDto;
+import com.spring.instafeed.auth.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/users")
 public class SignUpController {
+
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<AccessTokenResponseDto> signUp(@RequestBody SignUpRequestDto dto) {
+        AccessTokenResponseDto accessTokenResponseDto = authService.signUpUser(dto);
+        String token = accessTokenResponseDto.accessToken();
+
+        ResponseCookie cookie = ResponseCookie.from("token", token)
+                .httpOnly(true)
+                .secure(true)
+                .maxAge(60 * 60)
+                .build();
+
+        /**
+         * Todo: URI 추후에 결정
+         * ex)
+         * URI location = ServletUriComponentsBuilder
+         *                 .fromCurrentRequest()
+         *                 .path("") // 경로
+         *                 .buildAndExpand() // {} 넣을 값
+         *                 .toUri();
+         */
+
+        return ResponseEntity.created(URI.create(""))
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .body(accessTokenResponseDto);
+    }
 }
