5e3a7f30ecba183c5bccf8ce72516c0c8e74cedd
Groo <woohyeon3382@naver.com>
2024-12-24 09:39:36 +0900

    feat: JwtAuthenticationFilter 구현

---
+++ src/main/java/com/spring/instafeed/auth/web/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,62 @@
+package com.spring.instafeed.auth.web.filter;
+
+import com.spring.instafeed.auth.domain.TokenProvider;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.util.PatternMatchUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final TokenProvider tokenProvider;
+    private static final String[] WHITE_LIST = {"/users/signup", "/users/login"};
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = resolveToken(request);
+
+        // 토큰의 유효성 검사
+        if (token == null || !tokenProvider.validateToken(token)) {
+            throw new IllegalArgumentException("Invalid or missing token");
+        }
+
+        Long userId = tokenProvider.getUserId(token);
+        request.setAttribute("userId", userId);
+
+        filterChain.doFilter(request, response);
+    }
+
+    /**
+     * WHITE_LIST 의 경로는 Filter 를 무시함
+     */
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        String requestURI = request.getRequestURI();
+
+        for (String pattern : WHITE_LIST) {
+            if (PatternMatchUtils.simpleMatch(pattern, requestURI)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Request Header 에서 토큰을 가져옴
+     */
+    private String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+
+        return null;
+    }
+}
