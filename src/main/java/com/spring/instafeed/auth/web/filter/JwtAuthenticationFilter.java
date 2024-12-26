851240b7e277e8a53823b3c40c6ac600ed75aabb
Groo <woohyeon3382@naver.com>
2024-12-26 11:03:23 +0900

    chore: 불필요한 주석 제거

--- src/main/java/com/spring/instafeed/auth/web/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/spring/instafeed/auth/web/filter/JwtAuthenticationFilter.java
@@ -27,18 +27,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         }
 
         Long userId = tokenProvider.getUserId(token);
-        String requestURI = request.getRequestURI();
-
-        // 유저 유효성 검사
-        if (!userId.equals()) {
-            /**
-             * URI 에서 {id} 와 검증하는 방법
-             * - 의문점
-             * 1. URI 와 토큰의 userId 가 같은지 검증하는 방법이 보안상 이슈는 발생하지 않는지?
-             * 2. 실무에서 어떤 방식으로 처리하는지?
-             * 3. URI 검증 방법 외에 다른 방법은 어떤게 있는지?
-             */
-        }
 
         request.setAttribute("userId", userId);
 
