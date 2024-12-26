9ded12898da305c764b26c0f0a9264b68a397f33
backswan0 <rnrwprh3434@gmail.com>
2024-12-26 12:24:02 +0900

    style: 프로필 조회 시 식별자가 나오도록 매개변수 추가

--- src/main/java/com/spring/instafeed/profile/dto/response/ReadProfileResponseDto.java
+++ src/main/java/com/spring/instafeed/profile/dto/response/ReadProfileResponseDto.java
@@ -3,6 +3,7 @@ package com.spring.instafeed.profile.dto.response;
 import com.spring.instafeed.profile.entity.Profile;
 
 public record ReadProfileResponseDto(
+        Long id,
         String nickname,
         String content,
         String imagePath
@@ -13,8 +14,11 @@ public record ReadProfileResponseDto(
      * @param profile 변환할 Profile 엔티티 객체
      * @return Profile을 기반으로 생성된 QueryProfileResponseDto 객체
      */
-    public static ReadProfileResponseDto toDto(Profile profile) {
+    public static ReadProfileResponseDto toDto(
+            Profile profile
+    ) {
         return new ReadProfileResponseDto(
+                profile.getId(),
                 profile.getNickname(),
                 profile.getContent(),
                 profile.getImagePath()
