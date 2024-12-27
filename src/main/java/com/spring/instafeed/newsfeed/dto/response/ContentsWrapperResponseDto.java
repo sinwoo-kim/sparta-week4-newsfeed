249d0c7dac337b00fc97b0e2549686879d364424
sinwoo-kim <shinwoo5676@gmail.com>
2024-12-27 22:24:40 +0900

    fix: Newsfeed ContentWrapperResponseDto Class Type Class -> record

--- src/main/java/com/spring/instafeed/newsfeed/dto/response/ContentsWrapperResponseDto.java
+++ src/main/java/com/spring/instafeed/newsfeed/dto/response/ContentsWrapperResponseDto.java
@@ -5,8 +5,6 @@ import lombok.Getter;
 
 import java.util.List;
 
-@Getter
-@AllArgsConstructor
-public class ContentsWrapperResponseDto {
-    private final List<ReadNewsfeedResponseDto> contents; // "contents"라는 키를 가지는 필드
+public record ContentsWrapperResponseDto(List<ReadNewsfeedResponseDto> contents) {
+
 }
